b068ed23fd316034a044b3f323f69aa8abba00dc
Padraic <padraic.histon@gmail.com>
2017-04-28 10:10:37 +0100

    Added MembershipType model and MembershipType navigation property to Customer model. Updated database.

--- VideoShop/Models/Customer.cs
+++ VideoShop/Models/Customer.cs
@@ -9,5 +9,8 @@ namespace VideoShop.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public bool IsSubscribedToNewsletter { get; set; }
+        public MembershipType MembershipType { get; set; }
+        public byte MembershipTypeId { get; set; } // Entity Framework recongnizes this convention (MembershipTypeId) and treats this property as a Foreign Key.
     }
 }
