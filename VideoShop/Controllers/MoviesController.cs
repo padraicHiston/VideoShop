343f7cdeb83e118ebf9b73f7efb91e6e5916dcbb
Padraic <padraic.histon@gmail.com>
2017-04-28 09:25:18 +0100

    Added Index and Details pages for Customer and Movie.

--- VideoShop/Controllers/MoviesController.cs
+++ VideoShop/Controllers/MoviesController.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Collections.Generic;
 using System.Web.Mvc;
 using VideoShop.Models;
 using VideoShop.ViewModels;
@@ -10,24 +7,42 @@ namespace VideoShop.Controllers
 {
     public class MoviesController : Controller
     {
-        // GET: Movies
-        public ActionResult Random()
+        public ActionResult Index()
         {
-            var movie = new Movie() {Name = "Shrek"};
-            var customers = new List<Customer>()
-            {
-                new Customer() { Name = "Customer1"},
-                new Customer() { Name = "Customer2"}
-            };
+            var movies = GetMovies();
 
-            var viewModel = new RandomMovieViewModel()
-            {
-                Movie = movie,
-                Customers = customers
-            };
-
-            return View(viewModel);
+            return View(movies);
         }
+
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>()
+            {
+                new Movie() { Id = 1, Name = "Shrek"},
+                new Movie() { Id = 2, Name = "Wall-e"}
+            };
+        }
+
+
+
+        // GET: Movies
+        //public ActionResult Random()
+        //{
+        //    var movie = new Movie() {Name = "Shrek"};
+        //    var customers = new List<Customer>()
+        //    {
+        //        new Customer() { Name = "Customer1"},
+        //        new Customer() { Name = "Customer2"}
+        //    };
+
+        //    var viewModel = new RandomMovieViewModel()
+        //    {
+        //        Movie = movie,
+        //        Customers = customers
+        //    };
+
+        //    return View(viewModel);
+        //}
 
         //[Route("movies/released/{year:regex(\\d{4})}/{month:regex(\\d{2}):range(1, 12)}")]
         //public ActionResult ByReleaseYear(int year, int month)
