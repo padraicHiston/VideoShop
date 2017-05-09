c64f760f7dd3a8b023b93245e111757be5e762f3
Padraic <padraic.histon@gmail.com>
2017-05-09 10:25:50 +0100

    Added Web API and DTO for Customers.

---
+++ VideoShop/Dtos/CustomerDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using VideoShop.Models;
+
+namespace VideoShop.Dtos
+{
+    public class CustomerDto
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+
+        public bool IsSubscribedToNewsletter { get; set; }
+
+        public byte MembershipTypeId { get; set; } // Entity Framework recongnizes this convention (MembershipTypeId) and treats this property as a Foreign Key.
+
+        [Min18YearsIfAMember]
+        public DateTime? BirthDate { get; set; }
+    }
+}
